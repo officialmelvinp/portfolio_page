bd570385c33b8bf83d7708b879e78e81aac597a0
officialmelvinp <elmelvin@2002@gmail.com>
2024-12-16 09:05:12 +0100

    Update Django version and build configuration

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-python3.9 -m ensurepip
 python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',') + ['.vercel.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app']
 
 # Application definition
 INSTALLED_APPS = [
