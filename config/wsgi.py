ab5835e5837554ff01ff4a44576994a9bd5d8077
officialmelvinp <elmelvin@2002@gmail.com>
2024-12-16 08:14:41 +0100

    root project setup before deployment

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,8 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-application = get_wsgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
 
-def handler(event, context):
-    return application(event.get('headers', {}), event.get('body', ''))
+application = get_wsgi_application()
+app = application
