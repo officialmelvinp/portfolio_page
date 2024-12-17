63957484f26772426b2dcfa5895279a3e4354f36
officialmelvinp <elmelvin@2002@gmail.com>
2024-12-17 01:10:34 +0100

    updated setup for netlify

--- build_files.sh
+++ build_files.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-
-# Ensure pip is installed (if not already present)
-if ! python -m pip --version; then
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    python get-pip.py
-fi
-
-# Upgrade pip to the latest version
-python -m pip install --upgrade pip
-
-# Install dependencies from requirements.txt
-python -m pip install -r requirements.txt
-
-# Run collectstatic (no input required)
-python manage.py collectstatic --noinput
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput
--- vercel_handler.py
+++
@@ -1,8 +0,0 @@
-from django.core.wsgi import get_wsgi_application
-import os
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_page.settings')
-application = get_wsgi_application()
-
-def handler(request, **kwargs):
-    return application(request, **kwargs)
